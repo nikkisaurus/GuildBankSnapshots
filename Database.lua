fab322cda352713a305417cfda317943458ce01c
Niketa <niketa-wow@gmail.com>
2020-11-17 21:48:56 -0600

    Reset to version 3.0 bare bones

---
+++ Database.lua
@@ -0,0 +1,3 @@
+local addonName = ...
+local addon = LibStub("AceAddon-3.0"):GetAddon(addonName)
+local L = LibStub("AceLocale-3.0"):GetLocale(addonName, true)
---
+++ Locales/Locale-enUS.lua
@@ -0,0 +1,4 @@
+local addonName = ...
+local addon = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "AceEvent-3.0")
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "enUS", true)
+LibStub("LibAddonUtils-1.0"):Embed(addon)
