b017614c80948adb41e39658d5fa1165b84e0e15
Niketa <niketa-wow@gmail.com>
2020-11-18 02:31:51 -0600

    Cleanup code

--- Core.lua
+++ Core.lua
@@ -2,15 +2,31 @@ local addonName = ...
 local addon = LibStub("AceAddon-3.0"):GetAddon(addonName)
 local L = LibStub("AceLocale-3.0"):GetLocale(addonName, true)
 
+--*------------------------------------------------------------------------
+
 function addon:OnInitialize()
-    addon:RegisterEvent("GUILDBANKFRAME_OPENED")
-    addon:RegisterEvent("GUILDBANKFRAME_CLOSED")
+    self:InitializeDatabase()
+    -- self:RegisterEvent("GUILDBANKFRAME_CLOSED")
+    self:RegisterEvent("GUILDBANKFRAME_OPENED")
 end
+
+--*------------------------------------------------------------------------
+
+-- function addon:OnEnable()
+-- end
+
+------------------------------------------------------------
+
+-- function addon:OnDisable()
+-- end
+
+--*------------------------------------------------------------------------
+
+-- function addon:GUILDBANKFRAME_CLOSED()
+-- end
+
+------------------------------------------------------------
 
 function addon:GUILDBANKFRAME_OPENED()
     self:ScanGuildBank()
-end
-
-function addon:GUILDBANKFRAME_CLOSED()
-    -- idk if we need this or not
 end
--- Database.lua
+++ Database.lua
@@ -1,3 +1,9 @@
 local addonName = ...
 local addon = LibStub("AceAddon-3.0"):GetAddon(addonName)
 local L = LibStub("AceLocale-3.0"):GetLocale(addonName, true)
+
+--*------------------------------------------------------------------------
+
+function addon:InitializeDatabase()
+
+end
