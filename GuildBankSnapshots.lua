c7b55e6250f61074b588e6320e7cccd31107bf5b
Niketa <jlvellocido@gmail.com>
2023-02-02 04:02:28 -0600

    Remove if statement from EventUtil call

--- GuildBankSnapshots.lua
+++ GuildBankSnapshots.lua
@@ -17,34 +17,23 @@ function addon:OnInitialize()
     private:InitializeFrame()
     private:InitializeSlashCommands()
 
-    if select(4, GetBuildInfo()) >= 100000 then
-        EventUtil.ContinueOnAddOnLoaded("Blizzard_GuildBankUI", function()
-            addon:HookScript(_G["GuildBankFrame"], "OnShow", addon.GUILDBANKFRAME_OPENED)
-            addon:HookScript(_G["GuildBankFrame"], "OnHide", addon.GUILDBANKFRAME_CLOSED)
+    EventUtil.ContinueOnAddOnLoaded("Blizzard_GuildBankUI", function()
+        addon:HookScript(_G["GuildBankFrame"], "OnShow", addon.GUILDBANKFRAME_OPENED)
+        addon:HookScript(_G["GuildBankFrame"], "OnHide", addon.GUILDBANKFRAME_CLOSED)
 
-            if IsAddOnLoaded("ArkInventory") then
-                addon:HookScript(_G["ARKINV_Frame4"], "OnShow", addon.GUILDBANKFRAME_OPENED)
-                addon:HookScript(_G["ARKINV_Frame4"], "OnHide", addon.GUILDBANKFRAME_CLOSED)
-            elseif IsAddOnLoaded("Bagnon") then
-                addon:HookScript(_G["BagnonBankFrame1"], "OnShow", addon.GUILDBANKFRAME_OPENED)
-                addon:HookScript(_G["BagnonBankFrame1"], "OnHide", addon.GUILDBANKFRAME_CLOSED)
-            end
-
-            C_Timer.After(5, function()
-                -- Added delay because it seems some addons may be loading the guild bank UI before I have the data I need
-                private:UpdateGuildDatabase()
-            end)
-        end)
-    else
-        local _, _, _, loadable = GetAddOnInfo("Blizzard_GuildBankUI")
-        if loadable then
-            LoadAddOn("Blizzard_GuildBankUI")
+        if IsAddOnLoaded("ArkInventory") then
+            addon:HookScript(_G["ARKINV_Frame4"], "OnShow", addon.GUILDBANKFRAME_OPENED)
+            addon:HookScript(_G["ARKINV_Frame4"], "OnHide", addon.GUILDBANKFRAME_CLOSED)
+        elseif IsAddOnLoaded("Bagnon") then
+            addon:HookScript(_G["BagnonBankFrame1"], "OnShow", addon.GUILDBANKFRAME_OPENED)
+            addon:HookScript(_G["BagnonBankFrame1"], "OnHide", addon.GUILDBANKFRAME_CLOSED)
         end
 
-        addon:RegisterEvent("PLAYER_ENTERING_WORLD")
-        addon:RegisterEvent("GUILDBANKFRAME_CLOSED")
-        addon:RegisterEvent("GUILDBANKFRAME_OPENED")
-    end
+        C_Timer.After(5, function()
+            -- Added delay because it seems some addons may be loading the guild bank UI before I have the data I need
+            private:UpdateGuildDatabase()
+        end)
+    end)
 end
 
 function addon:OnEnable()
